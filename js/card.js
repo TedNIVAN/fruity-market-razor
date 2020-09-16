f9b5e0ead74d47c7a44c76af7329904bab717a19
Ted NIVAN <ted.jp.nivan@gmail.com>
2020-09-16 19:26:34 +0200

    Add files via upload

---
+++ js/card.js
@@ -0,0 +1,75 @@
+let cardsData = [{
+    id: 0,
+    name: 'Strawberry',
+    image: 'assets/png/028-strawberry.png',
+    price: 1,
+  },
+  {
+    id: 1,
+    name: 'Banana',
+    image: 'assets/png/006-bananas.png',
+    price: 2,
+  },
+  {
+    id: 2,
+    name: 'Pear',
+    image: 'assets/png/024-pear.png',
+    price: 2,
+  },
+  {
+    id: 3,
+    name: 'Dragon Fruit',
+    image: 'assets/png/015-dragon fruit.png',
+    price: 7,
+  },
+  {
+    id: 4,
+    name: 'Orange',
+    image: 'assets/png/022-orange.png',
+    price: 1,
+  },
+  {
+    id: 5,
+    name: 'Mango',
+    image: 'assets/png/021-mango.png',
+    price: 5,
+  },
+  {
+    id: 6,
+    name: 'Lemon',
+    image: 'assets/png/020-lemon.png',
+    price: 1,
+  },
+  {
+    id: 7,
+    name: 'Watermelon',
+    image: 'assets/png/030-watermelon.png',
+    price: 2,
+  },
+  {
+    id: 8,
+    name: 'Berry',
+    image: 'assets/png/008-berry.png',
+    price: 2,
+  },
+  {
+    id: 9,
+    name: 'Pomegranate',
+    image: 'assets/png/025-pomegranate.png',
+    price: 3,
+  },
+  {
+    id: 10,
+    name: 'Blueberry',
+    image: 'assets/png/009-blueberry.png',
+    price: 2,
+  },
+  {
+    id: 11,
+    name: 'Rose Apple',
+    image: 'assets/png/027-rose apple.png',
+    price: 4,
+  }
+  ]
+  
+ 
